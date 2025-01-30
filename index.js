9f33eb9df4a347d8fdc10aa0ca9e12826d33c3d4
sherif-abdoo <sherifabdulmaged@gmail.com>
2025-01-31 00:22:04 +0200

    cors added

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const usersRouter = require('./routes/users.router');
 const httpStatusCode = require('./utils/http.status');
+const cors = require('cors');
 
 
+app.use(cors());
 const app = express();
 app.use(express.json());
 const URL = process.env.MONGO_URL;
