8b9fe5dbb0b73cd099ae282c67ce92b4bfd4168e
sherif-abdoo <sherifabdulmaged@gmail.com>
2025-01-30 04:48:16 +0200

    added singin

--- routes/users.router.js
+++ routes/users.router.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const middleWare = require('../middleware/user.middleware');
 const userControllers = require('../controllers/user.controllers');
-
 router.route('/signUp')
-    .post(userControllers.signUp);
+    .post(middleWare.foundUser,middleWare.passwordEncryption,userControllers.signUp);
+
+router.route('/signIn')
+    .post(middleWare.checkInput,userControllers.signIn);
 
 module.exports = router;
