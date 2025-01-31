c5daca14484f3bc9ddcfc3c14d48e1be3cdfc8b8
AhmedMoEssam1512 <11410120236012@stud.cu.edu.eg>
2025-01-31 17:55:48 +0200

    lol

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,9 @@ const mongoose = require('mongoose');
 const usersRouter = require('./routes/users.router');
 const httpStatusCode = require('./utils/http.status');
 const cors = require('cors');
-
+const app = express();
 
 app.use(cors());
-const app = express();
 app.use(express.json());
 const URL = process.env.MONGO_URL;
 const PORT = process.env.PORT;
