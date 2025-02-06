9d501d6d6207f9d4fc12e97ffa09491160938d69
AhmedMoEssam1512 <11410120236012@stud.cu.edu.eg>
2025-02-06 11:10:49 +0200

    comment model added

---
+++ backend/models/comment.model.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const appError = require('../utils/app.error');
+const validator = require('validator');
+const User = require('./user.model');
+const post = require('./post.model');
+
+const commentSchema = new mongoose.Schema({
+    writer : {
+        type : mongoose.Schema.Types.ObjectId , 
+        ref : 'user' , 
+        required : true
+    },
+    content: {
+        type: String,
+        required : true,
+        maxLength: 500
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    likes: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+        required: false
+    }],
+    post:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'post',
+        required: true
+    }
+
+})
+
+
+module.exports = mongoose.model('post', postSchema);
