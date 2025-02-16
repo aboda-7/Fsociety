b6eb306cc76ae9fc96fa4f89ab3915437a145908
AhmedMoEssam1512 <11410120236012@stud.cu.edu.eg>
2025-02-16 13:34:36 +0200

    add comment fixed

--- backend/routes/post.router.js
+++ backend/routes/post.router.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const middleWare = require('../middleware/post.middleware');
 const postControllers = require('../controllers/post.controllers');
 const {protect} = require('../middleware/auth.middleware');
+const {checkComment} = require('../middleware/comment.middleware');
 
 router.route('/createPost')
     .post(protect, middleWare.postDataChecker, postControllers.createPost);
@@ -15,7 +16,7 @@ router.route('/likePost/:id')
     .patch(protect,middleWare.isPost, postControllers.likePost);
 
 router.route('/addComment/:id')
-    .patch(protect,middleWare.isPost, postControllers.addComment);
+    .patch(protect,middleWare.isPost,checkComment, postControllers.addComment);
    
 router.route('/deletePost/:id')
     .delete(protect,middleWare.isPost, middleWare.deletePost, postControllers.deletePost);
