503d322ea4a608c7bd4addb5bd8d0db799f07b73
AhmedMoEssam1512 <11410120236012@stud.cu.edu.eg>
2025-02-23 13:31:16 +0200

    follow model fixed

--- backend/models/follow.model.js
+++ backend/models/follow.model.js
@@ -4,13 +4,13 @@ const User = require('./user.model');
 const followSchema = new mongoose.Schema({
     follower :{
         type : mongoose.Schema.Types.ObjectId, 
-        ref = 'User',
-        required = true
+        ref : 'User',
+        required : true
     },
     followed : {
         type : mongoose.Schema.Types.ObjectId,
-        ref = 'User',   
-        required = true
+        ref : 'User',   
+        required : true
     },
     createdAt : {
         type : Date,
